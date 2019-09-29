d25f1a1c1bda3a0ee42e5f8658672cce33c8c662
David Urdaibay <urdaibad@tcd.ie>
2019-09-29 15:25:21 +0100

    Forgot to do gradual commits, finished exercise01 for cs3016

--- Exercise01/src/Ex01.hs
+++ Exercise01/src/Ex01.hs
@@ -1,4 +1,4 @@
--- Name: An On,  Username: anon
+-- Name: David Urdaibay,  Username: urdaibad 
 module Ex01 where
 import Data.Char (toUpper) -- needed for Part 1
 
@@ -10,7 +10,10 @@ Function 'toUpper :: Char -> Char' converts a character to uppercase
 if it is lowercase. All other characters are unchanged
 
 -}
-raise _ = error "raise not yet implemented"
+
+--raise :: [Char] -> [Char]
+raise [] = []
+raise (x : xs) = toUpper x : (raise xs)
 
 {- Part 2
 
@@ -18,7 +21,11 @@ Write a function 'nth' that returns the nth element of a list
 
 -}
 nth :: Int -> [a] -> a
-nth _ _ = error "raise not yet implemented"
+nth _ []          = error "empty list"
+nth n xs
+    | n < 0  = error "out of bounds"
+    | n >=0  = (!!) xs (n-1)
+
 
 {- Part 3
 
@@ -27,4 +34,9 @@ and reports the length of the prefix they have in common.
 
 -}
 commonLen :: Eq a => [a] -> [a] -> Int
-commonLen _ _ = error "commonLen not yet implemented"
+commonLen [] [] = 0
+commonLen [] _  = 0
+commonLen _ []  = 0
+commonLen (x:xs) (y:ys)
+    | x == y    = 1 + (commonLen xs ys)
+    | otherwise = 0
